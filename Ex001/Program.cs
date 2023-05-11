f862be0f06197825889e957554d74ebb41fcd61a
Romuljr <romulojunior252@gmail.com>
2023-05-11 16:07:23 -0300

    Adicionar arquivos de projeto.

---
+++ Ex001/Program.cs
@@ -0,0 +1,60 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Ex001.Entities;
+using Ex001.Repositories;
+
+Console.WriteLine("\n ~ CONTROLE DE CLIENTES ~ \n");
+
+var cliente = new Cliente();
+
+try
+{
+    Console.Write("INFORME O ID DO CLIENTE: ");
+    cliente.Id = int.Parse(Console.ReadLine());
+
+    Console.Write("INFORME O NOME DO CLIENTE: ");
+    cliente.Nome = Console.ReadLine();
+
+    Console.Write("INFORME O CPF DO CLIENTE: ");
+    cliente.Cpf = Console.ReadLine();
+
+    Console.Write("INFORME E-MAIL DO CLIENTE: ");
+    cliente.Email = Console.ReadLine();
+
+    var clienteRepository = new ClienteRepository();
+
+    clienteRepository.GravarDados(cliente);
+
+    Console.WriteLine("\n~ DADOS GRAVADOS COM SUCESSO ~");
+
+    Console.Write("\nDESEJA LER OS DADOS? [S/N]: ");
+    var opcao = Console.ReadLine();
+
+    if (opcao.ToUpper().Equals("S"))
+    {
+        var conteudo = clienteRepository.LerDados();
+        Console.WriteLine(conteudo);
+    }
+   
+    Console.WriteLine("\nFIM DO PROGRAMA");
+
+
+}
+catch (Exception e)
+{
+    Console.WriteLine("\nOCORREU UM ERRO: " + e.Message);
+
+    Console.Write("\nDESEJA TENTAR NOVAMENTE?[S/N]: ");
+    var opcao = Console.ReadLine();
+
+    if (opcao.ToUpper().Equals("S"))
+    {
+        Console.Clear();
+        //Consertar mais tarde kkk cansadao pprt
+    }
+    else
+    {
+        Console.WriteLine("\nFIM DO PROGRAMA");
+    }
+}
+
+Console.ReadKey();
---
+++ Ex001/Repositories/ClienteRepository.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Ex001.Entities;
+
+namespace Ex001.Repositories
+{
+    public class ClienteRepository
+    {
+        private string path = "c:\\temp\\clientes.txt";
+        public void GravarDados(Cliente cliente)
+        {
+            using(var streamWriter = new StreamWriter(path, true))
+            {
+                streamWriter.WriteLine("ID: " + cliente.Id);
+                streamWriter.WriteLine("NOME: " + cliente.Nome);
+                streamWriter.WriteLine("EMAIL: " + cliente.Email);
+                streamWriter.WriteLine("CPF: " + cliente.Cpf);
+                streamWriter.WriteLine("=-=-=-=<>=-=-=-=");
+
+            }
+        }
+        public string LerDados()
+        {
+            using (var streamReader = new StreamReader(path))
+            {
+                return streamReader.ReadToEnd();
+            }
+        }
+
+        internal void LerDados(Cliente cliente)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
